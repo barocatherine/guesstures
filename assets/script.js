565a86c1450c9944637912f9dcf6868a9d7d1228
nmisustin <nmisustin@gmail.com>
2021-04-05 16:10:26 -0700

    added fetch and checking emotions functions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,56 @@
 var faceImageSources = ["confused_1", "happy_1", "happy_2", "happy_3", "happy_4", "happy_5", "happy_6", "happy_7", "happy_8", "mad_1", "mad_2", "mad_3", "sad_1", "sad_2", "sad_3", "sad_4", "sad_5", "sad_6", "surprised_1", "surprised_2"];
+var emotions = [
+    {
+        uppercase: "Anger",
+        lowercase: "anger",
+        value: ""
+    },
+    {
+        uppercase: "Contempt",
+        lowercase: "contempt",
+        value: ""
+    },
+    {
+        uppercase: "Disgust",
+        lowercase: "disgust",
+        value: ""
+    },
+    {
+        uppercase: "Fear",
+        lowercase: "fear",
+        value: ""
+    },
+    {
+        uppercase: "Happiness",
+        lowercase: "happiness",
+        value: ""
+    },
+    {
+        uppercase: "Neutral",
+        lowercase: "neutral",
+        value: ""
+    },
+    {
+        uppercase: "Sadness",
+        lowercase: "sadness",
+        value: ""
+    },
+    {
+        uppercase: "Surprise",
+        lowercase: "surprise",
+        value: ""
+    },
+]
+var largestEmotion = "";
+var scoreWrapper = document.getElementById("score")
 var pictureWrapper = document.getElementById("picture-wrapper");
 var answerWrapper = document.getElementById("answer-wrapper");
 var startQuiz = document.getElementById("start-quiz");
 var questionNumber = 0;
+var score = 0
 var chosenFaces = [];
-
+// https://nmisustin.github.io/guesstures/assets/img/happy_1.jpg
+// this function creates a new array of different faces to use for the quiz
 function faceChooser(){
     var sources = faceImageSources.slice();
     for(var i = 0; i <6; i++){
@@ -16,76 +62,84 @@ function faceChooser(){
     }
 }
 faceChooser();
+//this function sends the image to the api to examine
 function emotionChecker(){
-    fetch()
+    const subscriptionKey = '18cd594249f24e20872d594ad4f7af5c';
+    var imageUrl = "https://nmisustin.github.io/guesstures/assets/img/"+chosenFaces[questionNumber]+".jpg"
+    var newUrl = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=emotion'
+    fetch(newUrl,{
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            'ocp-apim-subscription-key': subscriptionKey
+        },
+        body: JSON.stringify({url: imageUrl})
+    }).then(function(response){
+        response.json()
+        .then(function(data){
+            console.log(data);
+            //this loop takes the data from the api and puts it into an array
+            for(var i=0; i<emotions.length; i++){
+                var select = emotions[i].lowercase;
+                var value = data[0].faceAttributes.emotion[select];
+                emotions[i].value= value;
+            }
+            // this finds the highest number in the returned data and stores the object that it's in
+            largestEmotion = emotions.reduce(function(current, next){
+                if(next.value>current.value){
+                    return next
+                }
+                else{
+                    return current
+                }
+            })
+        })
+    })
 }
+//this function displays the quiz
 function displayQuiz(){
-    pictureWrapper.innerHTML="";
-    answerWrapper.innerHTML="";
-    var picture = document.createElement("img");
-    var anger = document.createElement("button");
-    anger.setAttribute("value","anger");
-    anger.setAttribute("type", "button");
-    anger.innerHTML = "Anger";
-    anger.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(anger);
-    var contempt = document.createElement("button");
-    contempt.setAttribute("value", "contempt");
-    contempt.setAttribute("type", "button");
-    contempt.innerHTML = "Contempt";
-    contempt.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(contempt);
-    var disgust = document.createElement("button");
-    disgust.setAttribute("value", "disgust");
-    disgust.setAttribute("type", "button");
-    disgust.innerHTML = "Disgust";
-    disgust.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(disgust);
-    var fear = document.createElement("button");
-    fear.setAttribute("value", "fear");
-    fear.setAttribute("type", "button");
-    fear.innerHTML = "Fear";
-    fear.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(fear);
-    var happiness = document.createElement("button");
-    happiness.setAttribute("value", "happiness");
-    happiness.setAttribute("type", "button");
-    happiness.innerHTML = "Happiness";
-    happiness.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(happiness);
-    var neutral = document.createElement("button");
-    neutral.setAttribute("value", "neutral");
-    neutral.setAttribute("type", "button");
-    neutral.innerHTML = "Neutral";
-    neutral.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(neutral);
-    var sadness = document.createElement("button");
-    sadness.setAttribute("value", "sadness");
-    sadness.setAttribute("type", "button");
-    sadness.innerHTML = "Sadness";
-    sadness.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(sadness);
-    var surprise = document.createElement("button");
-    surprise.setAttribute("value", "surprise");
-    surprise.setAttribute("type", "button");
-    surprise.innerHTML = "Surprise";
-    surprise.onclick = function(){checkAnswer()};
-    answerWrapper.appendChild(surprise);
-    console.log(chosenFaces[questionNumber])
-    picture.setAttribute("src", "./assets/img/"+chosenFaces[questionNumber]+".jpg");
-    pictureWrapper.appendChild(picture);
-    console.log(pictureWrapper)
+    if(questionNumber>5){
+        quizEnder();
+    }
+    else{
+        pictureWrapper.innerHTML="";
+        answerWrapper.innerHTML="";
+        scoreWrapper.innerHTML="";
+        scoreWrapper.innerHTML=score;
+        var picture = document.createElement("img");
+        //this loop creates all the buttons for the different emotion options
+        for(let i=0; i<emotions.length; i++){
+            var emotion = document.createElement("button");
+            emotion.setAttribute("type", "button");
+            emotion.innerHTML = emotions[i].uppercase;
+            emotion.onclick = function(){checkAnswer(emotions[i].lowercase)};
+            answerWrapper.appendChild(emotion);
+        }
+        console.log(chosenFaces[questionNumber])
+        picture.setAttribute("src", "./assets/img/"+chosenFaces[questionNumber]+".jpg");
+        pictureWrapper.appendChild(picture);
+        console.log(pictureWrapper)
+        console.log(questionNumber)
+        emotionChecker();
+    }
+    
 }
 //check answer and call next question
-function checkAnswer(){
+function checkAnswer(answer){
+    if(largestEmotion.lowercase===answer){
+        score++;
+    }
     questionNumber++;
     displayQuiz();
 }
-function gameEnder(){
+//this will be called when the quiz is over
+function quizEnder(){
     pictureWrapper.innerHTML="";
     answerWrapper.innerHTML="";
+    scoreWrapper.innerHTML="";
+    scoreWrapper.innerHTML=score;
     var endMessage = document.createElement("p");
     endMessage.innerHTML = "The quiz is over"
     pictureWrapper.appendChild(endMessage);
 }
-startQuiz.addEventListener("click", displayQuiz)
+startQuiz.onclick= function(){displayQuiz()}
